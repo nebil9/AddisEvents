568913c74da1afec5f9fad99434ea50584d29323
Rediet Teferra <rediet212@gmail.com>
2022-10-02 21:08:08 +0300

    Controller

--- AddisEvents/Controllers/CategoryController.cs
+++ AddisEvents/Controllers/CategoryController.cs
@@ -1,16 +1,76 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using AddisEvents.Models;
+using AddisEvents.Models.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AddisEvents.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly ICategoryService categoryService;
+
+        public CategoryController(ICategoryService service)
+        {
+            categoryService = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var categoryData = await categoryService.GetAllAsync();
+            return View(categoryData);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var categoryData = await categoryService.GetByIdAsync(id);
+            return View(categoryData);
+        }
+        public async Task<IActionResult> Create()
         {
             return View();
         }
+        [HttpPost]
+        public async Task<IActionResult> Create(Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+            await categoryService.AddAsync(category);
+            return RedirectToAction(nameof(Index));
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var data = await categoryService.GetByIdAsync(id);
+            if (data == null) return View("NotFound");
+            return View(data);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Category category)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(category);
+            }
+            if (id == category.Id)
+            {
+                await categoryService.UpdateAsync(id, category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var data = await categoryService.GetByIdAsync(id);
+            if (data == null) return View("NotFound");
+            return View(data);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var data = await categoryService.GetByIdAsync(id);
+            if (data == null) return View("NotFound");
+            await categoryService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
+
--- AddisEvents/Controllers/EventController.cs
+++ AddisEvents/Controllers/EventController.cs
@@ -1,16 +1,62 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace AddisEvents.Controllers
+﻿namespace AddisEvents.Controllers
 {
     public class EventController : Controller
     {
-        public IActionResult Index()
+        private readonly IEventService eventService;
+
+        public EventController(IEventService service)
         {
-            return View();
+            eventService = service;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await eventService.GetAllAsync();
+            return View(data);
+        }
+
+        public async Task<IActionResult> Details(int id)
+        {
+            var data = await eventService.GetByIdAsync(id);
+            if (data == null) return View("NotFound");
+            return View(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Event _event)
+        {
+            await eventService.AddAsync(_event);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            var data = await eventService.GetByIdAsync(id);
+            if (data == null) return View("NotFound");
+            return View(data);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Event product)
+        {
+            await eventService.UpdateAsync(id, product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var data = await eventService.GetByIdAsync(id);
+            if (data == null) return View("NotFound");
+            return View(data);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteConfirm(int id)
+        {
+            var data = await eventService.GetByIdAsync(id);
+            if (data == null) return View("NotFound");
+            await eventService.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
         }
     }
 }
